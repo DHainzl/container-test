dcfae01056cbb21b8c400514981042c4c4ce7b35
David Hainzl <david@dhainzl.at>
2019-11-06 15:30:24 +0100

    Initial commit

---
+++ src/main/java/at/dhainzl/spring/containertest/ContainerTestApplication.java
@@ -0,0 +1,13 @@
+package at.dhainzl.spring.containertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerTestApplication.class, args);
+	}
+
+}

efcf50f3f9068c76c3f3ad92b9cc51c080f65156
David Hainzl <david@dhainzl.at>
2019-11-07 08:33:03 +0100

    Adds customizable hello

--- src/main/java/at/dhainzl/spring/containertest/rest/HelloWorld.java
+++ src/main/java/at/dhainzl/spring/containertest/rest/HelloWorld.java
@@ -1,13 +1,14 @@
 package at.dhainzl.spring.containertest.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
     @GetMapping("api/hello")
-    public String getGreeting() {
-        return "Hello, World!";
+    public String getGreeting(@RequestParam(required = true) String name) {
+        return "Hello, " + name + "!";
     }
 }
